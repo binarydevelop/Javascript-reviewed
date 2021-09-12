b9c43dd730d19c472d4e13f7f69d8039559dad63
Tushar Roy <binarydevelop@gmail.com>
2021-09-12 09:50:52 +0530

    Solution: Variables

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-alert('FROM External Script Source')
+alert('FROM External Script Source');
+
+/* Variables Solution */
+let name = "John" ;
+let admin = name;
+alert(admin);
+
+let OurPlanet= "Earth"
+let currentVisitor = "John";
